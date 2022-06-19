29f1a33d60e09c492f79a51a96c7d57189d4b50e
Wionixx <60029322+Wionixx@users.noreply.github.com>
2022-06-19 22:16:41 +0300

    Add files via upload

---
+++ script/script.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/halushko/appz_bot_example.git
+cd appz_bot_example
+apt update
+apt install maven -y
+
+mvn clean install && cd hello_bot && mvn exec:java -Dexec.mainClass="kpi.acts.appz.bot.hellobot.HelloWorldBot" -Dexec.args="5464040760:AAHLyoKTJzN_R4_P7wFG8qxEsnTRvQLfKjA lab2_team1_bot"
