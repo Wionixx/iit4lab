bed07f596a77733ca6b42b609feec3b21cea9871
Wionixx <60029322+Wionixx@users.noreply.github.com>
2022-06-20 00:06:46 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@ git clone https://github.com/halushko/appz_bot_example.git
 cd appz_bot_example
 apt update
 apt install maven -y
-123
+
 mvn clean install && cd hello_bot && mvn exec:java -Dexec.mainClass="kpi.acts.appz.bot.hellobot.HelloWorldBot" -Dexec.args="5464040760:AAHLyoKTJzN_R4_P7wFG8qxEsnTRvQLfKjA lab2_team1_bot"
